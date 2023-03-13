ed93b845ca8a4604104cdd08247ae1aa1f3628da
Ebenezer Jesuraj <ebenezer@beezlabs.com>
2023-03-13 11:19:45 +0530

    WIP: v0.1 : added resources for spring rest service CRUD app

---
+++ 04-CRUD-App/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into CrawlerDefinition values(10001,'bacardi', 'E1234567', 'E1234568', 'E1234569', 'E1234560');
+
+insert into CrawlerData values(10002, 10002, 'tulip', 'A1234568', 'A1234569', 'A1234560', 'A1234561');
